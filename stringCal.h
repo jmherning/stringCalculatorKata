34bc6ac8f98bcded4856c30c5ba8b1a4bc3a2f8a
Jason <jmherning93@gmail.com>
2019-02-22 15:04:26 -0900

    pass single number return test

--- stringCal.h
+++ stringCal.h
@@ -2,6 +2,7 @@
 // Created by Jason on 2/15/2019.
 //
 
+
 #ifndef KATASTRINGCAL_STRINGCAL_H
 #define KATASTRINGCAL_STRINGCAL_H
 
--- stringCal_test.cpp
+++ stringCal_test.cpp
@@ -2,6 +2,7 @@
 // Created by Jason on 2/15/2019.
 //
 
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "stringCal.h"
 
